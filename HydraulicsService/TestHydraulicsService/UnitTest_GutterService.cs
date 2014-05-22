988b0ac7a5bab3d0c7ee9b61d24ed705785ae536
huangt <tony.huang@autodesk.com>
2014-05-22 14:48:52 +0800

    Update from vs2012 to vs2013.
    Modify the UT.

--- HydraulicsService/TestHydraulicsService/UnitTest_GutterService.cs
+++ HydraulicsService/TestHydraulicsService/UnitTest_GutterService.cs
@@ -18,14 +18,13 @@ namespace TestHydraulicsService
             gd.CrossSlopeX = 0.02;
             gd.CrossSlopeW = 0.05;
             gd.GutterWidth = 4.0;
-            double GutterKc = 0.56, ManningRoughness = 0.016, DesignFlow = 3.0, GrateWidth = 2.0;
+            double ManningRoughness = 0.016, DesignFlow = 3.0;
             GutterService service = new GutterService();
-            GutterResult gr = service.GutterCalculation(GutterKc, ManningRoughness, gd, DesignFlow, GrateWidth);
+            GutterResult gr = service.GutterCalculation_DesignFlow(ManningRoughness, gd, DesignFlow);
             Assert.AreEqual(7.293, gr.GutterSpread, 0.003);
             Assert.AreEqual(0.266, gr.GutterDepth, 0.001);
             Assert.AreEqual(3.885, gr.GutterVelocity, 0.001);
             Assert.AreEqual(0.942, gr.Eo, 0.001);
-            Assert.AreEqual(0.662, gr.EoEx, 0.001);
         }
     }
 }
